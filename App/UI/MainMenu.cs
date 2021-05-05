1bd1c0a8b452c7ee4741ba17b11991429b8d3532
phobos42 <garrettruffner42@gmail.com>
2021-05-05 13:37:12 -0400

    UI begins

---
+++ App/UI/MainMenu.cs
@@ -0,0 +1,40 @@
+using System;
+using Serilog;
+namespace UI
+{
+    public class MainMenu : IMenu
+    {
+
+        public void Start()
+        {
+            bool repeat = true;
+            do{
+                Console.WriteLine("Welcome!");
+                Console.WriteLine("[0] Add R");
+                Console.WriteLine("[1] Add Review");
+                Console.WriteLine("[2] exit");
+                string input = Console.ReadLine();
+                switch (input)
+                {
+                    case "0":
+
+                    break;
+                    case "1":
+                       
+                    break;
+                    case "2":
+                        Console.WriteLine("Bye");
+                        Log.Information("program exit from menu");
+                        repeat = false;
+                        Log.CloseAndFlush();
+                    break;
+                    default:
+                    Console.WriteLine("Choose valid option");
+                    break;
+
+                }
+            } while(repeat);
+            
+        }
+    }
+}
---
+++ App/UI/UI.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Serilog;
+namespace UI
+{
+    class UI
+    {
+        static void Main(string[] args)
+        {
+            Log.Logger = new LoggerConfiguration()
+                .MinimumLevel.Debug()
+                .WriteTo.File("App/logs/app.txt", rollingInterval: RollingInterval.Day)
+                .CreateLogger();
+
+            Log.Information("Hello, world!");
+
+            int a = 10, b = 0;
+            try
+            {
+                Log.Debug("Dividing {A} by {B}", a, b);
+                Console.WriteLine(a / b);
+            }
+            catch (Exception ex)
+            {
+                Log.Error(ex, "Something went wrong");
+            }
+            Console.WriteLine("Hello World!");
+
+            IMenu menu = new MainMenu();
+            menu.Start();
+            Log.Information("menu Started");
+
+        }
+    }
+}
