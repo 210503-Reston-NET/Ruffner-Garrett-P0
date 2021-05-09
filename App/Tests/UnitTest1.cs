400213e6e88653835c7539babb956ee032ad07b0
phobos42 <garrettruffner42@gmail.com>
2021-05-09 00:34:27 -0400

    Started Unit tests

--- App/Tests/UnitTest1.cs
+++ App/Tests/UnitTest1.cs
@@ -1,14 +1,27 @@
 using System;
 using Xunit;
+using UI;
+using Service;
 
 namespace Tests
 {
     public class UnitTest1
     {
         [Fact]
-        public void Test1()
+        public void CityValidationTest()
         {
-
+            
+            Assert.Equal(ValidationService.ValidateCityName("St. Louis"), true);
+            Assert.Equal(ValidationService.ValidateCityName("city #4"),false);
         }
+        [Fact]
+        public void NameValidationTest()
+        {
+            Assert.Equal(ValidationService.ValidatePersonName("Daniel McDowell"), true);
+            Assert.Equal(ValidationService.ValidatePersonName("sk8r boi"),false);
+            Assert.Equal(ValidationService.ValidatePersonName(" "),false);
+            Assert.Equal(ValidationService.ValidatePersonName(""),false);
+        }
+        
     }
 }
